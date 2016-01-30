2c37305e69e723b7062fd8e3b23bb36dc69f6a91
Pavol Safarik <pavol.safarik@gmail.com>
2016-01-30 13:55:15 +0100

    works

--- platforms/android/assets/www/js/config.js
+++ platforms/android/assets/www/js/config.js
@@ -5,8 +5,10 @@
 
 //var wsHost = "172.16.128.113";
 //var wsPort = "8081";
-var wsHost = "10.48.18.174";
-var wsPort = "8085";
+//var wsHost = "10.48.18.174";
+//var wsPort = "8085";
+var wsHost = "";
+var wsPort = "";
 
 
 /////////////////////////////
